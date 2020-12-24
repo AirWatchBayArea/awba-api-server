2599c396cea83fd335eb904b665169a82447ef0b
John Bowman <eflyer43@gmail.com>
2020-12-23 23:39:57 -0500

    Remove default path

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 import requests
-import socket
+# import socket
 from fastapi import FastAPI
 from .routes import esdr_v1
 
 app = FastAPI(debug=True, title="AWBA API Server", description="This provides AWBA API functionality", version="0.0.0b")
 app.include_router(esdr_v1.router, prefix="/api/v1")
-hostname = socket.gethostname()
+# hostname = socket.gethostname()
 
-@app.get("/", include_in_schema=False)
-async def read_root():
-    return {
-        "name": "AWBA API Server",
-        "host": hostname, 
-        "version": "1.0"
-    }
+# @app.get("/", include_in_schema=False)
+# async def read_root():
+#     return {
+#         "name": "AWBA API Server",
+#         "host": hostname, 
+#         "version": "1.0"
+#     }
