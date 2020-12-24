046e91acc781f3c91361df10731c06eda3dcace1
John Bowman <eflyer43@gmail.com>
2020-12-23 22:52:32 -0500

    Adding root info

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 import hypercorn
 import requests
+import socket
 from fastapi import FastAPI
 from .routes import esdr_v1
 
 app = FastAPI(debug=True, title="AWBA API Server", description="This provides AWBA API functionality", version="0.0.0b")
 app.include_router(esdr_v1.router, prefix="/api/v1")
+hostname = socket.gethostname()
+
+@app.get("/")
+async def read_root():
+    return {
+        "name": "AWBA API Server",
+        "host": hostname, 
+        "version": "1.0"
+    }
