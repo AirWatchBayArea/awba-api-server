cffa5b52bc7c577cbd5b966ccd8435fe3d1f0150
John Bowman <eflyer43@gmail.com>
2021-01-20 21:55:51 -0500

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .backend.database import Database
 from .routes import feeds, locations
 
@@ -19,11 +20,21 @@ app = FastAPI(debug=True,
     title="AWBA API Server", 
     version="1.0.0")
 
+origins = ["*"]
+
 @app.on_event("startup")
 async def Verify_Database():
     # Make sure the tables exist
     database = Database()
     database.Verify_Tables()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=[],
+)
 
 app.include_router(feeds.router)
 app.include_router(locations.router)
